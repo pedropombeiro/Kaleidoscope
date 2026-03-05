8e5c37138598722650a82a289cfc75938e0c5b1e
Pedro Pombeiro <ppombeiro@gitlab.com>
2026-03-05 16:57:19 +0100

    Move Caps Lock to left Prog key, revert META layer and right corner changes

--- examples/Devices/Keyboardio/Model100/Model100.ino
+++ examples/Devices/Keyboardio/Model100/Model100.ino
@@ -185,14 +185,14 @@ enum {
 KEYMAPS(
 
   [PRIMARY] = KEYMAP_STACKED
-  (Key_Escape,   Key_1, Key_2, Key_3, Key_4, Key_5, Key_RightGui,
+  (Key_CapsLock, Key_1, Key_2, Key_3, Key_4, Key_5, Key_RightGui,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_Home,     Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_End,      Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    TD(TD_LCTRL), Key_Backspace, TD(TD_LCMD), TD(TD_LSHIFT),
    ShiftToLayer(META),
 
-   LCTRL(LGUI(Key_LeftAlt)), Key_6,       Key_7,        Key_8,     Key_9,      Key_0,         Key_CapsLock,
+   LCTRL(LGUI(Key_LeftAlt)), Key_6,       Key_7,        Key_8,     Key_9,      Key_0,         XXX,
    Key_Enter,                Key_Y,       Key_U,        Key_I,     Key_O,      Key_P,         Key_Equals,
                              Key_H,       Key_J,        Key_K,     Key_L,      Key_Semicolon, Key_Quote,
    LGUI(Key_Spacebar),       Key_N,       Key_M,        Key_Comma, Key_Period, Key_Slash,     Key_Minus,
@@ -240,7 +240,7 @@ KEYMAPS(
    ___,               Key_F6,                     Key_F7,                              Key_F8,                              Key_KeypadLeftParen,         Key_KeypadRightParen, ___,
    ___,               ___,                        Key_LeftCurlyBracket,                Key_RightCurlyBracket,               Key_LeftBracket,             Key_RightBracket,     ___,
                       LCTRL(LALT(Key_LeftArrow)), LCTRL(Key_DownArrow),                LCTRL(Key_UpArrow),                  LCTRL(LALT(Key_RightArrow)), ___,                  ___,
-   Key_CapsLock,      ___,                        Consumer_DisplayBrightnessDecrement, Consumer_DisplayBrightnessIncrement, Consumer_PlaySlashPause,     Key_Backslash,        Key_Pipe,
+   ___,               ___,                        Consumer_DisplayBrightnessDecrement, Consumer_DisplayBrightnessIncrement, Consumer_PlaySlashPause,     Key_Backslash,        Key_Pipe,
    ___, ___, ___, ___,
    ___)
 ) // KEYMAPS(
